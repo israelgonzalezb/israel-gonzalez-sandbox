53b64ab5eefdeadc9fe4d7e0accb74d179527e49
Israel Gonzalez-Brooks <48233351+israelgonzalezb@users.noreply.github.com>
2021-10-28 21:33:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const fs = require('fs');
 // node-fetch ^3.0 doesn't allow require
 const fetch = require('cross-fetch');
 
+/*
+check if customer json file exists
+make customers
+put data in json file
+html file has script to append table
+when user submits search, search module executes
+
+*/
+
+if (!fs.existsSync("./customers.json")) {
+    // --
+}
 
 // Sync prevents the app from serving index early
 const indexFile = fs.readFileSync('./index.html');
