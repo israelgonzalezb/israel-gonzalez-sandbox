d6a45d4ded2ea55106e45f00c3885e46c0c9d236
Israel Gonzalez-Brooks <48233351+israelgonzalezb@users.noreply.github.com>
2021-10-29 11:48:10 -0700

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const generateNames = async () => {
 
   let wikiResponses = await Promise.allSettled(stringArr.splice(0,20).map(sortStart => fetch(
     `${corsUrl}${wikiFirstNameUrl}${sortStart.join("")}`
-  ).then((res) => res.json()).catch(err => console.log("!!!", err))));
+  ).then((res) => res.text()).catch(err => console.log("!!!", err))));
  return wikiResponses//.map(name => name.value.query.categorymembers[0].title);
 
 }
