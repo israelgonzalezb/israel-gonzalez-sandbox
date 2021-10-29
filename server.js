9d27b3a292d64ea4837a2e885c5c5bb51d79c094
Israel Gonzalez-Brooks <48233351+israelgonzalezb@users.noreply.github.com>
2021-10-29 10:48:24 -0700

    Update server.js

--- server.js
+++ server.js
@@ -34,8 +34,7 @@ const generateNames = async () => {
   });
 
 
-  let results = [...list];
-  let continueKey = next;
+ 
 
   const corsUrl = process.env.CORS_URL;
   const wikiFirstNameUrl = `https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category:Given_names&cmtype=page&cmnamespace=0&format=json&cmlimit=1&cmprop=title|sortkey&cmstartsortkeyprefix=dft`;
