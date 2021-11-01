a835e09e9ee5255234bc5428232844a0494046e4
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2021-11-01 10:34:59 -0700

    fix: change customer creation script

--- createCustomers.js
+++ createCustomers.js
@@ -1,9 +1,7 @@
 const fetch = require('cross-fetch');
 const { customers } = require('./customers.json');
-const base64 = require("./base64");
 
 const createCustomers = async (token, data = customers) => {
-  let auth = base64(`${process.env.DWOLLA_KEY}:${process.env.DWOLLA_SECRET}`);
   data.forEach((customer) => {
     fetch('https://api-sandbox.dwolla.com/customers', {
       method: 'POST',
@@ -11,9 +9,9 @@ const createCustomers = async (token, data = customers) => {
         'Content-Type': 'application/json',
         Accept: 'application/vnd.dwolla.v1.hal+json',
         Authorization: `Bearer ${token}`,
-        'content-length': customers[1].length,
+        'content-length': customer.length,
       },
-      body: JSON.stringify(customers[1]),
+      body: JSON.stringify(customer),
     })
       .then((res) => res.text())
       .then((res) => console.log(res))
